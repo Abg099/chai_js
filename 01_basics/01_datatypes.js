9e9bed76c990f60173ad06fc377026525ce210a7
abhishek09 <157100194+Abg099@users.noreply.github.com>
2025-09-11 13:14:43 +0000

    datatypes primitive and non-primitive learn

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,23 @@
+// we can see 2 types of data types here one is primitive(call by value) and another one is non-primitive that is (call by reference)
+//1.null 2.BigInt 3.boolean 4.string 5.undefined 6.Number 7.symbol( primitive datatypes)
+
+//Array,Object,function are the non- primitive datatypes.Array
+
+//we define array as :-
+const arr =["shakti","deva","hero"]
+console.log(arr)
+
+//we define onject as
+const heros={
+eName:"hitesh",
+eAge:"33"
+ //we write object in key value pair
+}
+console.log(heros)
+
+//we define arrays as
+const myFunction = function(){
+    
+    console.log("hello javascript");
+}
+myFunction();
