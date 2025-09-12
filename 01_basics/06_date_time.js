56ae94ecb82030fcba975671d44151791bbcafed
abhishek09 <157100194+Abg099@users.noreply.github.com>
2025-09-12 12:10:19 +0000

    date and time done

---
+++ 01_basics/06_date_time.js
@@ -0,0 +1,23 @@
+let myDate = new Date();
+//console.log(myDate);
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toLocaleTimeString());
+
+// you can give customize date 
+
+let createdDate = new Date("02-05-2024") //user given date
+console.log(createdDate.toLocaleString());
+
+let timeStamp = Date.now()
+console.log(timeStamp); // used to calculate time , to know time difference of submission between two users
+
+
+
+
+
+
+
