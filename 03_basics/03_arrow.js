c96baa4a54f77a5b239962857751f6bc08378ce4
abhishek09 <157100194+Abg099@users.noreply.github.com>
2025-09-15 10:18:16 +0000

    arrow function done

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,10 @@
+//syntax of arrow function
+
+//const addTwo = (num1 , num2) =>{
+    //return num1 + num2 //explicit
+//}
+//console.log(addTwo(3 ,5));
+
+const addTwo = (num1 , num2) =>(num1 + num2) //implicit return you can write arrow functionslike this also
+//console.log(addTwo(3 ,5));
+
