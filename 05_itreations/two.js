daa3fec3d17a3e577e95243de9c623234bc3cc5b
abhishek09 <157100194+Abg099@users.noreply.github.com>
2025-09-17 09:10:09 +0000

    while and do while loop done

---
+++ 05_itreations/two.js
@@ -0,0 +1,13 @@
+//while loop 
+let i = 0;
+while (i < 10) {
+    //console.log(`index is ${i}`);
+    i++;
+}
+
+//do while loop
+let score = 0;
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score < 5);
